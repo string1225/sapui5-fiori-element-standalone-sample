b8997fd77285bf61d9cf2a19f6b8777542a793a7
String Shi <junteng007@live.cn>
2020-03-02 14:02:57 +0800

    2020-03-02 Initial with Web IDE template

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("fe_sample.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
